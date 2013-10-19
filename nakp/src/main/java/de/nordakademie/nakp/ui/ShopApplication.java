a9e920d3d124132b47dc62a1aa1a97cc591e92a2
Jonas Tebbe <jonastebbe@googlemail.com>
2013-10-19 12:01:32 +0200

    config of web.xml

---
+++ nakp/src/main/java/de/nordakademie/nakp/ui/ShopApplication.java
@@ -0,0 +1,13 @@
+package main.java.de.nordakademie.nakp.ui;
+
+import org.apache.wicket.Page;
+import org.apache.wicket.protocol.http.WebApplication;
+
+public class ShopApplication extends WebApplication {
+
+	@Override
+	public Class<? extends Page> getHomePage() {
+		return null;
+	}
+
+}
